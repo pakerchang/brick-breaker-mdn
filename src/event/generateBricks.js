ab36ffafbe56ccd65a6208207953fef74a7eff46
pakerchang <pakerchang@gmail.com>
2023-01-03 17:26:07 +0800

    ref(generate): Refactor bricks generate function.

--- src/event/generateBricks.js
+++ src/event/generateBricks.js
@@ -1,11 +1,7 @@
 const generateBrick = (bricksData) => {
   let { brickColCount, brickRowCount, bricks } = bricksData;
-  for (let col = 0; col < brickColCount; col++) {
-    bricks[col] = [];
-    for (let row = 0; row < brickRowCount; row++) {
-      bricks[col][row] = { x: 0, y: 0, status: 1 };
-    }
-  }
+  const rows = new Array(brickRowCount).fill({ x: 0, y: 0, status: 1 });
+  bricks = new Array(brickColCount).fill(rows);
   return bricks;
 };
 
